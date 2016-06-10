9ab63fb5a2fd768a2b82bf6472cfa540ecbde321
Nothen <hardwick101@gmail.com>
2016-06-10 21:54:25 +0100

    Removed random files and created python file to check ping with file write

---
+++ DoThis/PingTest.py
@@ -0,0 +1,24 @@
+import time, re, random
+from subprocess import check_output
+
+
+hostname = "4.2.2.2"
+
+ran = random.randrange(1000)
+while True:
+    # output = subprocess.Popen(["ping.exe",hostname],stdout = subprocess.PIPE).decode('utf-8')
+    out = check_output(["ping", "-n", "1",hostname]).decode('utf-8')
+    # x = re.match('[0-9]+ms', out)
+    x = re.search('[0-9]+ms', out)
+    y = x.group(0)
+    print(out)
+    # print(y)
+
+    # z = (time.strftime("%d/%m/%Y at %H:%M") + " Ping:" + y + "\n")
+    #
+    # with open("ping_record.txt","a") as b:
+    #     b.write(z)
+    #
+    time.sleep(1)
+
+# " + str(ran) + " use this if you want to generate a random name for the text file, pretty pointless
